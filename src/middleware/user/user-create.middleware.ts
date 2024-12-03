3a612e00e89600ea9211138279cd00b48aa5bd78
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-12-03 14:09:30 -0300

    feat: add base create user

---
+++ src/middleware/user/user-create.middleware.ts
@@ -0,0 +1,11 @@
+import { NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+/*export class userCreateMiddleware implements NestMiddleware{
+  use(req: Request, res: Response, next: NextFunction): {
+    const data = req.body;
+    if(!(data.name))
+  }
+}
+
+ */
