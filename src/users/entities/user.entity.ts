1aa9a53f29dceffda045bcc8cb42a90cebae9b75
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-12-03 14:57:37 -0300

    feat: create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+import { Status } from '../../enums/status.enum';
+
+export class User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  status: Status;
+  created_at?: Date;
+  updated_at?: Date;
+}
