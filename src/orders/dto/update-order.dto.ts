848b5e6577e7035959b149109f0c0db0b395bd0f
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-12-05 20:13:33 -0300

    feat: create update order DTO

--- src/orders/dto/update-order.dto.ts
+++ src/orders/dto/update-order.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { OrderSaveDTO } from './order-save.dto';
 
-export class UpdateOrderDto extends PartialType(OrderSaveDTO){}
+export class UpdateOrderDto extends PartialType(OrderSaveDTO) {}
