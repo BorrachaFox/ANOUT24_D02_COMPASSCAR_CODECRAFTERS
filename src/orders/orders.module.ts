22e7e886ad8232bd5cb9605c31fc248d74a626c9
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-12-05 19:51:47 -0300

    feat: add dependences client and car

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders-service';
+import { ClientsModule } from '../clients/clients.module';
+import { CarsModule } from '../cars/cars.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ClientsModule, CarsModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
