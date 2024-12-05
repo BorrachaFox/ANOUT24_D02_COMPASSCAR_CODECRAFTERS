f45ff9bfb2dacc1c81181bdf0236e0134a397e74
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-12-05 15:13:22 -0300

    feat: create custom decorator for array unique

---
+++ src/Decorators/cars/car-items-unique.decorator.ts
@@ -0,0 +1,22 @@
+import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
+
+export function IsArrayUnique( validationOptions?: ValidationOptions) {
+  return function( object: object, propertyKey: string ) {
+    registerDecorator({
+      name: 'isArrayUnique',
+      target: object.constructor,
+      propertyName: propertyKey,
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          if (!Array.isArray(value)) return false;
+          const uniqueItems = new Set(value);
+          return uniqueItems.size === value.length;
+        },
+        defaultMessage(): string {
+          return 'Items must not contain duplicate values.';
+        },
+      },
+    });
+  };
+}
