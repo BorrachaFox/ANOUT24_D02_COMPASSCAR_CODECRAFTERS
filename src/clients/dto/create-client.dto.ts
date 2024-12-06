52d9ace49ca05b0fc03c80c7d04a0a6e22c7917d
Luiz Guilherme <luiz.camargo.pb@compasso.com.br>
2024-12-06 14:27:35 -0300

    feat: Add description and examples/format in create-cars.dto

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -9,19 +9,20 @@ import {
 } from 'class-validator';
 
 export class CreateClientDto {
-  @ApiProperty({ description: "Add the customer's full name" })
+  @ApiProperty({ description: "Add the customer's full name." })
   @IsString()
   name: string;
 
   @ApiProperty({
-    description: 'Add customer CPF',
+    description: 'Add customer CPF.',
     example: '111.444.777-35',
   })
   @IsString()
   cpf: string;
 
   @ApiProperty({
-    description: "Add the customer's date of birth",
+    description: "Add the customer's date of birth.",
+    format: 'yyyy-mm-dd',
     example: '2000-12-01',
   })
   @Type(() => Date)
@@ -29,7 +30,7 @@ export class CreateClientDto {
   birthday: string;
 
   @ApiProperty({
-    description: 'Add customer email',
+    description: 'Add customer email.',
     example: 'qwerty@gmail.com',
   })
   @IsEmail()
