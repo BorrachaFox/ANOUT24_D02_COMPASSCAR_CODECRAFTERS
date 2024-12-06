f2f3eac17805b79f23ed27ff28b580b3321cd7be
Luiz Guilherme <luiz.camargo.pb@compasso.com.br>
2024-12-06 14:53:34 -0300

    feat: Add description and examples in create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
 
 export class CreateUserDTO {
+  @ApiProperty({ description: "Add the user's full name." })
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'Add user email.',
+    example: 'qwerty@gmail.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: `
+    User's password. Requirements:
+    - At least 8 characters.
+    - At least 1 number.
+    - The number of uppercase letters, lowercase letters and symbols are optional.
+  `,
+    example: 'password123',
+  })
   @IsStrongPassword({
     minLength: 8,
     minLowercase: 0,
